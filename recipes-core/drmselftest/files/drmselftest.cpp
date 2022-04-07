5d607e8792eda4228cda61281d98d9b5d453a497
jbleclere <jbleclere@accelize.com>
2022-04-07 16:09:47 +0000

    Update source header

--- recipes-core/drmselftest/files/drmselftest.cpp
+++ recipes-core/drmselftest/files/drmselftest.cpp
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2021-2021 ProvenRun S.A.S
+ * Copyright (c) 2022 Accelize
  * All Rights Reserved.
  *
  * This software is the confidential and proprietary information of
- * ProvenRun S.A.S ("Confidential Information"). You shall not
+ * Accelize ("Confidential Information"). You shall not
  * disclose such Confidential Information and shall use it only in
  * accordance with the terms of the license agreement you entered
- * into with ProvenRun S.A.S
+ * into with Accelize
  *
- * PROVENRUN S.A.S MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+ * ACCELIZE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
  * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
  * BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. PROVENRUN S.A.S SHALL
+ * FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ACCELIZE SHALL
  * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
  * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
  */
